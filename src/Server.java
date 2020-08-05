21ed78be74d697e3c58bf940d38d29ce04421b04
HusamSaleem <60799172+HusamSaleem@users.noreply.github.com>
2020-08-05 10:57:18 -0700

    C

--- src/Server.java
+++ src/Server.java
@@ -9,20 +9,30 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		
 		ServerSocket serverSocket = new ServerSocket(5014);
-		Socket socket = serverSocket.accept();
+		Socket clientSocket = serverSocket.accept();
 		System.out.println(serverSocket.toString());
-		System.out.println(socket.getRemoteSocketAddress());
+		System.out.println(clientSocket.getRemoteSocketAddress());
 		
 		System.out.println("Client Connected!");
 		
-		InputStreamReader in = new InputStreamReader(socket.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
+		int red = -1;
+		byte[] buffer = new byte[5*1024]; // a read buffer of 5KiB
+		byte[] redData;
+		StringBuilder clientData = new StringBuilder();
+		String redDataText;
+		while ((red = clientSocket.getInputStream().read(buffer)) > -1) {
+		    redData = new byte[red];
+		    System.arraycopy(buffer, 0, redData, 0, red);
+		    redDataText = new String(redData,"UTF-8"); // assumption that client sends data UTF-8 encoded
+		    System.out.println("message part recieved:" + redDataText); 
+		    clientData.append(redDataText);
+		}
+		System.out.println("Data From Client :" + clientData.toString());
 		
-		System.out.println(bf.readLine());
-		socket.sendUrgentData(-1);
+		clientSocket.sendUrgentData(-1);
 		
 		serverSocket.close();
-		socket.close();
+		clientSocket.close();
 	}
 
 }
