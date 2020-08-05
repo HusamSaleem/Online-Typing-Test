6cc84b08efd163484fafdd6f42804a928c017a91
HusamSaleem <60799172+HusamSaleem@users.noreply.github.com>
2020-08-05 14:18:31 -0700

    g

--- src/Server.java
+++ src/Server.java
@@ -5,36 +5,66 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-	
+
 	static ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+	static ServerSocket serverSocket;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		ServerSocket serverSocket = new ServerSocket(5014);
+		Server server = new Server();
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				server.shutDown();
+			}
+		});
+
+		serverSocket = new ServerSocket(5014);
 		System.out.println("Server is listening on port: " + serverSocket.getLocalPort());
-		
+
 		while (true) {
 			// Accepts any connections
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Client Connected: " + clientSocket.toString());
-			
+
 			// Add client to existing client list
 			ClientHandler cl = new ClientHandler(clientSocket);
 			Thread thread = new Thread(cl);
-			
+
 			thread.start();
 			clients.add(cl);
+			
+			deleteInactiveClients();
 		}
+		
+	}
+	
+	private static void deleteInactiveClients() {
+		for (ClientHandler c : clients) {
+			if (c.s.isClosed() || !c.s.isConnected()) {
+				clients.remove(c);
+			}
+		}
+	}
+	
+	private void shutDown() {
+		System.out.println("Shutting down the server...");
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
 
 class ClientHandler extends Thread {
-	final Socket s;
-	
+	public final Socket s;
+
 	public ClientHandler(Socket s) {
 		this.s = s;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
@@ -43,7 +73,7 @@ class ClientHandler extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		while (!s.isClosed()) {
 			try {
 				System.out.println("Client: " + s.toString() + " sent this data: " + recieveData());
@@ -58,7 +88,7 @@ class ClientHandler extends Thread {
 				break;
 			}
 		}
-		
+
 		try {
 			s.close();
 		} catch (IOException e) {
@@ -66,7 +96,7 @@ class ClientHandler extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void sendData(String data) throws IOException {
 		System.out.println("Sending data");
 		PrintWriter writer = new PrintWriter(s.getOutputStream());
@@ -74,22 +104,21 @@ class ClientHandler extends Thread {
 		writer.flush();
 		System.out.println("Data has been sent!");
 	}
-	
+
 	public String recieveData() throws IOException, InterruptedException {
 		int red = -1;
-		byte[] buffer = new byte[5*1024]; // A read buffer of 5 KiB
+		byte[] buffer = new byte[5 * 1024]; // A read buffer of 5 KiB
 		byte[] redData;
-		
+
 		StringBuilder clientData = new StringBuilder();
 		String redDataText;
-		
+
 		// While there is still data available
 		while ((red = s.getInputStream().read(buffer)) > -1) {
 			redData = new byte[red];
 			System.arraycopy(buffer, 0, redData, 0, red);
-			
+
 			redDataText = new String(redData, "UTF-8"); // Assuming the client sends UTF-8 Encoded
-			System.out.println(redDataText);
 			clientData.append(redDataText);
 		}
 		return clientData.toString();
