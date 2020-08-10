6a5b0f16141c30715f403c199b8380f2bd0c8ece
HusamSaleem <60799172+HusamSaleem@users.noreply.github.com>
2020-08-10 16:51:35 -0700

    asd

--- src/ServerPackage/MysqlConn.java
+++ src/ServerPackage/MysqlConn.java
@@ -24,11 +24,13 @@ public class MysqlConn {
 	
 	private void startConnection() {
 		try { 
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			dbConn = DriverManager.getConnection(url, username, password);
 			
 			if (dbConn != null)
 				System.out.println("Connectionn has been established!");
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			System.out.println(e.getMessage());
 		} 
 	}
