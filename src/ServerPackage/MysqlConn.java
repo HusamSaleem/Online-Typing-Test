e20bcefdceb8cafa86b88c85fa8118a5cb613323
HusamSaleem <60799172+HusamSaleem@users.noreply.github.com>
2020-08-11 11:19:21 -0700

    a

--- src/ServerPackage/MysqlConn.java
+++ src/ServerPackage/MysqlConn.java
@@ -7,38 +7,38 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MysqlConn {
-	
+
 	Connection dbConn = null;
-	
+
 	private String url = "jdbc:mysql://localhost:3306/TypingTestDb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private String username = "PotatoAdmin";
 	private String password = "Popakon123!";
-	
+
 	public MysqlConn() {
-		
+
 		startConnection();
 		displayAccounts();
 		System.out.println(registerUsername("Test"));
 	}
-	
+
 	private void startConnection() {
-		try { 
-			
+		try {
+
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			dbConn = DriverManager.getConnection(url, username, password);
-			
+
 			if (dbConn != null)
 				System.out.println("Database Connection has been established!");
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
-	
+
 	private void displayAccounts() {
 		try {
 			Statement statement = dbConn.createStatement();
 			ResultSet result = statement.executeQuery("SELECT * FROM accounts ORDER BY ID");
-			
+
 			System.out.println("----------ALL ACCOUNTS CURRENTLY REGISTERED----------");
 			while (result.next()) {
 				// Retrieve the column for each row
@@ -46,26 +46,31 @@ public class MysqlConn {
 				System.out.println("ID: " + result.getInt("ID") + ", Name: " + name);
 			}
 			
+			System.out.println("-----------------------------------------------------");
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	private boolean registerUsername(String user) {
 		try {
 			Statement statement = dbConn.createStatement();
-			ResultSet result = statement.executeQuery("INSERT INTO accounts(Name) VALUES('" + user + "')");
-			
-			return true;
-			
+			int result = statement.executeUpdate("INSERT INTO accounts(Name) VALUES('" + user + "')");
+
+			if (result > 0)
+				return true;
+			else
+				return false;
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 			return false;
 		}
-		
+
 	}
 
 }
